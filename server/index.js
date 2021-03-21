76ec7b190f8e24fe4b639ef176ec6ed557570a75
YaybeNo <evanmatt.wong@gmail.com>
2021-03-20 20:40:09 -0400

    Added mysql connection

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const mysql = require('mysql');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let con = mysql.createConnection({
+    host: process.env.con,
+    user: process.env.user,
+    password: process.env.pass
+  });
 
 app.listen(PORT, () => {
     console.log('listening')
