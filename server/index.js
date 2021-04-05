1991c7a764b950be2f68e41342d25657b5f885a8
YaybeNo <evanmatt.wong@gmail.com>
2021-04-05 11:23:05 -0400

    Trigger Heroku deploy after enabling collectstatic

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
   next();
 });
