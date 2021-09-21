b1d5e73300bdffb98a70a8fa78b600b58044a3e3
unknown <evanmatt.wong@gmail.com>
2021-09-21 00:10:42 -0400

    docker containers successfully communicates with each other

--- serverpy/app.py
+++ serverpy/app.py
@@ -7,14 +7,16 @@ from neo4j import GraphDatabase
 from dotenv import dotenv_values
 config = dotenv_values('.env')
 
-uri = config['URI']
-driver = GraphDatabase.driver(uri, auth=(config['USER'], config['PASS']))
+db_host = os.environ.get('NEO4J_HOST', '172.19.0.2')
+db_port = os.environ.get('NEO4J_PORT', '7687')
+print(f'bolt://{db_host}:{db_port}')
+driver = GraphDatabase.Driver(f'bolt://{db_host}:{db_port}')
 
 app = Flask(__name__)
 CORS(app)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
 
 @app.route('/')
 def hello():
@@ -26,36 +28,9 @@ def hello():
 def match_players(tx, player1, player2):
     return list(tx.run("match (m:Player {name: $Player1 }), (n:Player {name: $Player2 }), p=shortestPath((m)-[*]-(n)) return p", Player1 = player1, Player2 = player2))
 
-def query_database(player1, player2):
+def parseConnections(connections):
     results = []
-    connections = []
-
-    try:
-        with driver.session() as session:    
-            connections = session.read_transaction(match_players, player1, player2)
-
-    except :
-      print('something went wrong')
-      return '', 404; 
-    #print connections: 
-    # [<Record p=<Path start=<Node id=5660 labels=frozenset({'Player'}) properties={'name': 'lebron james'}> end=<Node id=4882 labels=frozenset({'Player'}) properties={'name': 'james harden'}> size=4>>]
-
-    #print(type(connections[0])) 
-    #<Record p=<Path start=<Node id=5660 labels=frozenset({'Player'}) properties={'name': 'lebron james'}> end=<Node id=4882 labels=frozenset({'Player'}) properties={'name': 'james harden'}> size=4>>
-    #<class 'neo4j.data.Record'>
-
-    #print((connections[0]['p']))
-    #<Path start=<Node id=5660 labels=frozenset({'Player'}) properties={'name': 'lebron james'}> end=<Node id=4882 labels=frozenset({'Player'}) properties={'name': 'james harden'}> size=4>
-    #<class 'neo4j.graph.Path'>
-
-    #print(list(connections[0]['p'])[0])
-
-    # print(connections)
-
-    # for record in connections:
-    #     print(type(record[0].nodes))    
-
-
+    print(connections)
     for i in connections[0]['p']:   
 
         for j in range(len((i.nodes))):
@@ -82,6 +57,48 @@ def query_database(player1, player2):
 
     return jsonify(res)
 
+
+def query_database(player1, player2):
+    connections = []
+
+    try:
+        with driver.session() as session:    
+            connections = session.read_transaction(match_players, player1, player2)
+
+    except:
+        print('something went wrong')
+        return Response('something went wrong', status=404)
+
+    if len(connections) == 0:
+        return 'No connections found',204 # if nothing about connections
+    else:
+        try:
+            return Response(parseConnections(connections), status=400, mimetype='application/json')
+        except:
+            print("Something went wrong")
+            return Response(status=404)
+
+    #print connections: 
+    # [<Record p=<Path start=<Node id=5660 labels=frozenset({'Player'}) properties={'name': 'lebron james'}> end=<Node id=4882 labels=frozenset({'Player'}) properties={'name': 'james harden'}> size=4>>]
+
+    #print(type(connections[0])) 
+    #<Record p=<Path start=<Node id=5660 labels=frozenset({'Player'}) properties={'name': 'lebron james'}> end=<Node id=4882 labels=frozenset({'Player'}) properties={'name': 'james harden'}> size=4>>
+    #<class 'neo4j.data.Record'> 
+
+    #print((connections[0]['p']))
+    #<Path start=<Node id=5660 labels=frozenset({'Player'}) properties={'name': 'lebron james'}> end=<Node id=4882 labels=frozenset({'Player'}) properties={'name': 'james harden'}> size=4>
+    #<class 'neo4j.graph.Path'>
+
+    #print(list(connections[0]['p'])[0])
+
+    # print(connections)
+
+    # for record in connections:
+    #     print(type(record[0].nodes))    
+
+
+
+
 @app.route('/getConnections', methods=['POST'])
 def get_connections():    
     if request.method == 'POST':
@@ -90,8 +107,6 @@ def get_connections():
         
         print(player1 + ' ' + player2);
         results = query_database(player1,player2)
-
-        return results
 
     
 
